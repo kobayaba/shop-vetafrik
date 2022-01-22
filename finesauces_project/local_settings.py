db957d769ee5266da47258cc5bf0db975f4e0ce9
kobayaba <yabakoba@gmail.com>
2022-01-22 14:42:41 +0000

    initial commit

---
+++ finesauces_project/local_settings.py
@@ -0,0 +1,15 @@
+SECRET_KEY = ''
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+	'default':{
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'finesauces',
+		'USER': 'finesaucesadmin',
+		'PASSWORD': '09212607Fd',
+		'HOST': 'localhost'
+	}
+}
